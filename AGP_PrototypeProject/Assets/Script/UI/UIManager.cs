b8caf3e09db4150a06dcf92e5045618669d174b5
DESKTOP-2DRV2SH\robne <robneir@gmail.com>
2017-01-21 13:21:32 -0800

    Added ugly ass command panel but it is there.

---
+++ AGP_PrototypeProject/Assets/Script/UI/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Inputs;
+
+/*
+ * author: rob neir
+ * date: 1/20/2017 
+ * 
+ * */
+namespace UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        public CommandPanel CommandPanel;
+
+        public void SelectCommand(InputPacket inputPacket)
+        {
+            // Make sure the command panel is active before passing command selections to it.
+            if (CommandPanel.gameObject.activeSelf)
+            {
+                CommandPanel.SelectCommand(inputPacket);
+            }
+        }
+    }
+}
