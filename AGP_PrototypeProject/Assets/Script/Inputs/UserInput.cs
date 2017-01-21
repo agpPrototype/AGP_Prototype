5481191e39fd6beb5550eb3f5d4e146bd26eea3f
Sam Khong <sam.khong92@gmail.com>
2017-01-20 23:07:48 -0800

    input stuff cuz rob is needy

--- AGP_PrototypeProject/Assets/Script/Inputs/UserInput.cs
+++ AGP_PrototypeProject/Assets/Script/Inputs/UserInput.cs
@@ -4,27 +4,132 @@ using UnityEngine;
 using InControl;
 using Utility;
 
-public class UserInput : MonoBehaviour {
-
-    private InputDevice m_device = InputManager.ActiveDevice;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void FixedUpdate()
+namespace Inputs
+{
+    public class UserInput : MonoBehaviour
     {
-        GetInputs();
+        private InputPacket[] m_inputArray;
+        private InputDevice m_device = InputManager.ActiveDevice;
+        private Queue<InputPacket> m_InputPacketQueue;
         
-    }
+        public InputPacket[] InputPackets
+        {
+            get
+            {
+                return m_inputArray;
+            }
+        }
 
-    void GetInputs()
-    {
+        void Awake()
+        {
+            m_inputArray = new InputPacket[16];
+            m_InputPacketQueue = new Queue<InputPacket>();
+        }
 
+        void FixedUpdate()
+        {
+            GetInputs();
+
+        }
+
+        void GetInputs()
+        {
+            m_InputPacketQueue.Clear();
+            {
+                float amount = m_device.LeftStickX;
+                InputPacket packet = new InputPacket(EnumService.InputType.LeftStickX, amount);
+                m_inputArray[(int)EnumService.InputType.LeftStickX] = packet;
+            }
+
+            {
+                float amount = m_device.LeftStickY;
+                InputPacket packet = new InputPacket(EnumService.InputType.LeftStickY, amount);
+                m_inputArray[(int)EnumService.InputType.LeftStickY] = packet;
+            }
+          
+            {
+                float amount = m_device.RightStickX;
+                InputPacket packet = new InputPacket(EnumService.InputType.RightStickX, amount);
+                m_inputArray[(int)EnumService.InputType.RightStickX] = packet;
+            }
+
+            {
+                float amount = m_device.RightStickY;
+                InputPacket packet = new InputPacket(EnumService.InputType.RightStickY, amount);
+                m_inputArray[(int)EnumService.InputType.RightStickY] = packet;
+            }
+
+            {
+                float amount = m_device.LeftBumper;
+                InputPacket packet = new InputPacket(EnumService.InputType.LB, amount);
+                m_inputArray[(int)EnumService.InputType.LB] = packet;
+            }
+
+            {
+                float amount = m_device.RightBumper;
+                InputPacket packet = new InputPacket(EnumService.InputType.RB, amount);
+                m_inputArray[(int)EnumService.InputType.RB] = packet;
+            }
+
+            {
+                float amount = m_device.LeftTrigger;
+                InputPacket packet = new InputPacket(EnumService.InputType.LT, amount);
+                m_inputArray[(int)EnumService.InputType.LT] = packet;
+            }
+
+            {
+                float amount = m_device.RightTrigger;
+                InputPacket packet = new InputPacket(EnumService.InputType.RT, amount);
+                m_inputArray[(int)EnumService.InputType.RT] = packet;
+            }
+
+            {
+                float amount = m_device.DPadUp;
+                InputPacket packet = new InputPacket(EnumService.InputType.DUp, amount);
+                m_inputArray[(int)EnumService.InputType.DUp] = packet;
+            }
+
+            {
+                float amount = m_device.DPadDown;
+                InputPacket packet = new InputPacket(EnumService.InputType.DDown, amount);
+                m_inputArray[(int)EnumService.InputType.DDown] = packet;
+            }
+
+            {
+                float amount = m_device.DPadLeft;
+                InputPacket packet = new InputPacket(EnumService.InputType.DLeft, amount);
+                m_inputArray[(int)EnumService.InputType.DLeft] = packet;
+            }
+
+            {
+                float amount = m_device.DPadRight;
+                InputPacket packet = new InputPacket(EnumService.InputType.DRight, amount);
+                m_inputArray[(int)EnumService.InputType.DRight] = packet;
+            }
+
+            {
+                float amount = m_device.Action1;
+                InputPacket packet = new InputPacket(EnumService.InputType.X, amount);
+                m_inputArray[(int)EnumService.InputType.X] = packet;
+            }
+
+            {
+                float amount = m_device.Action2;
+                InputPacket packet = new InputPacket(EnumService.InputType.O, amount);
+                m_inputArray[(int)EnumService.InputType.O] = packet;
+            }
+
+            {
+                float amount = m_device.Action3;
+                InputPacket packet = new InputPacket(EnumService.InputType.Square, amount);
+                m_inputArray[(int)EnumService.InputType.Square] = packet;
+            }
+
+            {
+                float amount = m_device.Action4;
+                InputPacket packet = new InputPacket(EnumService.InputType.Triangle, amount);
+                m_inputArray[(int)EnumService.InputType.Triangle] = packet;
+            }
+        }
     }
 }
--- AGP_PrototypeProject/Assets/Script/Utility/EnumService.cs
+++ AGP_PrototypeProject/Assets/Script/Utility/EnumService.cs
@@ -7,22 +7,22 @@ namespace Utility {
 
         public enum InputType
         {
-            LeftStickX,
-            LeftStickY,
-            RightStickX,
-            RightStickY,
-            LB,
-            LT,
-            RB,
-            RT,
-            DUp,
-            DDown,
-            DLeft,
-            DRight,
-            X,
-            O,
-            Triangle,
-            Square
+            LeftStickX = 0,
+            LeftStickY = 1,
+            RightStickX = 2,
+            RightStickY = 3,
+            LB = 4,
+            LT = 5,
+            RB = 6,
+            RT = 7,
+            DUp = 8,
+            DDown = 9,
+            DLeft = 10,
+            DRight = 11,
+            X = 12,
+            O = 13,
+            Triangle = 14,
+            Square = 15
         }
     }
 }
