e02e876e783a21f308be487c746184fa529b5243
Sam Khong <sam.khong92@gmail.com>
2017-01-20 20:34:36 -0800

    some basic scripts, folder

---
+++ AGP_PrototypeProject/Assets/Script/PlayerControl/UserInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class UserInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        InputDevice device = InputManager.ActiveDevice;
+
+    }
+}
