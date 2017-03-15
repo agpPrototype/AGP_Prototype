031dd05c31e37c8cdda29a9d923680d514252f05
Michael Feng <fengm@usc.edu>
2017-03-15 13:19:55 -0700

    Accalia's magic now varies with the bond status. In general, to apply an effect from the changing bond status, inherit from BondEffect and override the DoEffect() function, the bondmanager will call this function whenever the bond status changes.

---
+++ AGP_PrototypeProject/Assets/Script/Bond/BondEffects/BondSliderEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BondSliderEffect : BondEffect {
+
+	[SerializeField]
+	private Slider BondBar;
+
+	public override void DoEffect() 
+	{
+		if (!BondBar)
+		{
+			Debug.LogError("BondBar missing from scene");
+			return;
+		}
+
+		int newBond = Bond.BondManager.Instance.BondStatus;
+
+		BondBar.GetComponent<Slider>().value = newBond;
+	}
+}
