8f8a5f8d70fb883d7373bd6bd9f9e895dfa449c6
DESKTOP-2DRV2SH\robne <robneir@gmail.com>
2017-03-15 02:38:19 -0700

    added main menu scene

---
+++ AGP_PrototypeProject/Assets/Script/UI/Menus/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/// <summary>
+/// author: Rob Neir
+/// </summary>
+/// 
+
+namespace UI
+{
+    public class MainMenu : Menu
+    {
+
+        public string m_StartingLevel = "";
+
+        public void NewGameClicked()
+        {
+            SceneManager.LoadScene(m_StartingLevel);
+        }
+
+        public void SettingsClicked()
+        {
+            Debug.Log("Settings Button Clicked");
+        }
+
+        public void LoadGameClicked()
+        {
+            SceneManager.LoadScene(m_StartingLevel);
+        }
+
+        public void QuitClicked()
+        {
+            Application.Quit();
+        }
+    } // MainMenu class
+} // UI namespace
