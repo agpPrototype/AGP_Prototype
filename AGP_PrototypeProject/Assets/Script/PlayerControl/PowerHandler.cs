a583e046d0d3806713905fc63f0e30edab39e3f3
Sam Sintz <samsintz@gmail.com>
2017-03-14 17:42:37 -0700

    Adding SmokeVision Effect -> mapped to triangle

---
+++ AGP_PrototypeProject/Assets/Script/PlayerControl/PowerHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCritical;
+using Utility;
+using System;
+
+namespace Player
+{
+    public class PowerHandler : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+
+
+        public void ProcessPowers(PCActions pca)
+        {
+            if (pca.InputPackets[(int)EnumService.InputType.Triangle] != null)
+            {
+                pca.SmellSmoke = Convert.ToBoolean(pca.InputPackets[(int)EnumService.InputType.Triangle].Value);
+            }
+            DoActions(pca);
+        }
+
+
+        private void DoActions(PCActions pca)
+        {
+            if (pca.SmellSmoke)
+            { 
+                GameController.Instance.SmellSmokeDriver.ToggleSmellSmoke();
+            }
+        }
+    }
+}
+
