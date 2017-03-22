0e763866d127ab9286471ad96cf31e76ee39d72e
Sam Khong <sam.khong92@gmail.com>
2017-03-22 01:39:54 -0700

    added cinematic ending (need to finish emplemeting ending logic)

---
+++ AGP_PrototypeProject/Assets/Script/AnimSMB/CrumbleSMB.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrumbleSMB : StateMachineBehaviour
+{
+    float maxTime = 12f;
+    float compiledTime = 0.0f;
+    Transform clone;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        clone = animator.gameObject.transform;
+    }
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        compiledTime += Time.deltaTime;
+        if (compiledTime >= maxTime)
+        {
+            //imator.SetBool("End", true);
+            animator.Stop();
+        }
+        else
+        {
+            clone.position = new Vector3(clone.position.x, clone.position.y - 1.2f * Time.deltaTime, clone.position.z);
+        }
+    }
+}
